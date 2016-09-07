efe26c63004ea11da9de08fcad951e542e5c5652
Jenly <jenly1314@vip.qq.com>
2016-09-07 15:45:20 +0800

    新增注释

--- src/main/java/com/king/base/adapter/ViewPagerFragmentAdapter.java
+++ src/main/java/com/king/base/adapter/ViewPagerFragmentAdapter.java
@@ -1,6 +1,5 @@
-package com.king.base.adapter;
-
-/*Copyright 2015, 2016 Jenly Yu <a href="mailto:jenly1314@gmail.com">Jenly</a>
+/*
+ Copyright © 2015, 2016 Jenly Yu <a href="mailto:jenly1314@gmail.com">Jenly</a>
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -13,7 +12,10 @@ package com.king.base.adapter;
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
+
  */
+package com.king.base.adapter;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
